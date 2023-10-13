a85cc6ef33dbc5ede55e41d0009ac745ab13b6fd
Alfath Izha Barikallah <alfathizhaaaaaaa@gmail.com>
2023-10-13 10:51:06 +0700

    layout adjustment

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,34 +15,50 @@ export default function RootLayout({ children }) {
           </NavBar>
           {children}
         </main>
-        <footer className='w-full  border-t border-gray-700 bg-main-black pb-32'>
-          <div className='flex md:flex-row flex-col justify-between mx-4 '>
+        <footer className='w-full border-t border-gray-700 bg-main-black pb-32'>
+          <div className='md:flex md:justify-center md:gap-32 grid grid-cols-2 mx-auto'>
             {/* first footer */}
             <div className='px-4 mt-4'>
-              <h1 className='text-[28px] text-white font-black'>NexT-Component.</h1>
-              <p className='text-gray-400 md:text-base text-sm ml-2 mt-2'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, natus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, quasi?</p>
+              <h1 className='text-base text-white font-black'>NexT-Component.</h1>
             </div>
             {/* second footer */}
             <div className='px-4 mt-4'>
-              <h1 className='font-bold text-[34px]'>Quick Links.</h1>
-              <ul className='list-none text-gray-400 ml-2'>
+              <h1 className='font-semibold text-base'>Resources</h1>
+              <ul className='list-none text-gray-400'>
                 <li>
-                  <Link className='hover:text-gray-200 duration-500 md:text-base text-sm' href={"/about"}>About</Link>
+                  <Link className='hover:text-gray-200 duration-500  text-sm' href={"/about"}>About</Link>
                 </li>
                 <li>
-                  <Link className='hover:text-gray-200 duration-500 md:text-base text-sm' href={"#"}>Guide</Link>
+                  <Link className='hover:text-gray-200 duration-500  text-sm' href={"#"}>Guide</Link>
                 </li>
                 <li>
-                  <Link className='hover:text-gray-200 duration-500 md:text-base text-sm' href={"#"}>Design</Link>
+                  <Link className='hover:text-gray-200 duration-500  text-sm' href={"#"}>Design</Link>
                 </li>
                 <li>
-                  <Link className='hover:text-gray-200 duration-500 md:text-base text-sm' href={"#"}>Documentation</Link>
+                  <Link className='hover:text-gray-200 duration-500  text-sm' href={"#"}>Docs</Link>
                 </li>
               </ul>
             </div>
             {/* third footer */}
             <div className='px-4 mt-4'>
-              <h1 className='font-bold text-[34px]'>Contacts.</h1>
+              <h1 className='font-semibold text-base'>More</h1>
+              <ul className='text-gray-400'>
+                <li className='hover:text-gray-200 duration-560  text-sm'>
+                  Github
+                </li>
+                <li className='hover:text-gray-200 duration-560  text-sm'>
+                  Release
+                </li>
+                <li className='hover:text-gray-200 duration-500  text-sm'>
+                  <a href="https://github.com/justizha/NextDesignComponent/releases" className='cursor-pointer' target='_blank'>
+                    Learn Next js
+                  </a>
+                </li>
+              </ul>
+            </div>
+            {/* fourth footer */}
+            <div className='px-4 mt-4'>
+              <h1 className='font-semibold text-base'>Contacts.</h1>
               <ul className='list-none'>
 
               </ul>
@@ -51,7 +67,7 @@ export default function RootLayout({ children }) {
           </div>
           <div className='flex justify-between relative top-20  items-center border-t border-gray-600'>
             <div className='mx-10 mt-10'>
-              <p className='text-gray-400 text-xs md:text-base'>©Alfath Izha Barikallah</p>
+              <p className='text-gray-400 text-xs '>©Alfath Izha Barikallah</p>
             </div>
             <div className='mx-10 mt-10'>
               <div className='flex gap-4 items-center'>
