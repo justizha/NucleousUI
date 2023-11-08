8933e655f987858013c1cc886f4ef7aee4b77daa
justizha <alfathizhaaaaaaa@gmail.com>
2023-11-08 10:00:07 +0700

    api setup

--- src/app/sections/Faq.tsx
+++ src/app/sections/Faq.tsx
@@ -1,8 +1,18 @@
 'use client';
 
 import { HelpCircle } from "lucide-react";
-import faq from "./faq.json";
-export default function Faq() {
+import { useEffect, useState } from "react";
+
+export default function data() {
+    const [data, setData] = useState(null)
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/justizha/json-db/main/faq.json')
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data)
+                console.log(data)
+            })
+    }, [])
     return (
         <section className="lg:pt-20 lg:pb-20 pb-16 pt-16 z-10 bg-main-black">
             <div className="">
@@ -11,14 +21,14 @@ export default function Faq() {
                 </h1>
             </div>
             <div className="pt-5 grid gap-4 md:grid-cols-2 mx-4">
-                {faq.map((faq) => (
-                    <div className="bg-[#101128] p-2 rounded" key={faq.id}>
+                {data && data.map((item: any) => (
+                    <div className="bg-[#101128] p-2 rounded" key={item.id}>
                         <h2 className="text-base font-semibold flex  mb-2 gap-1 text-main-white">
                             <HelpCircle className="w-4" />
-                            {faq.question}
+                            {item.question}
                         </h2>
                         <p className="text-sm text-start  text-main-gray">
-                            {faq.answers}
+                            {item.answers}
                         </p>
                     </div>
                 ))}
