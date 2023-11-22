5d6c4a79bb1aba4d72c4ea86885be05eccc292da
justizha <alfathizhaaaaaaa@gmail.com>
2023-11-22 16:55:35 +0700

    removing unnecessary files

--- src/app/underdev/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Dev() {
-    return (
-        <div>
-            <a href="https://imgflip.com/i/7yk45g" className="object-contain">
-                <img src="https://i.imgflip.com/7yk45g.jpg" title="made at imgflip.com" className="w-screen h-screen object-contain" />
-            </a>
-        </div>
-    )
-}
