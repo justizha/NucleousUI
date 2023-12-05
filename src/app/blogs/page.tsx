53b3d59b3f5d134f88badd95d45829b090f7043b
justizha <alfathizhaaaaaaa@gmail.com>
2023-12-05 14:38:01 +0700

    get

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,20 +1,30 @@
 
 import prisma from "../lib/prisma"
 
+export async function getStaticProps() {
 
-export default async function BLogSection() {
     const blogs = await prisma.blog.findMany()
+
+    return {
+        props: {
+            blogs
+        }
+    }
+}
+
+
+export default async function BLogSection({ blogs }) {
     return (
         <div className="grid md:grid-cols-3 gap-3 grid-cols-1">
             <div className="w-full px-4">
-                {blogs.map((blog) => (
-                    <div className="mx-auto w-full max-w-2x rounded shadow shadow-black  border border-main-gray bg-gray-600 text-main-white p-2">
+                <div className="mx-auto w-full max-w-2x rounded shadow shadow-black  border border-main-gray bg-gray-600 text-main-white p-2">
+                    {blogs.map((blog) => (
                         <ul key={blog.id}>
                             <li>{blog.title}</li>
                             <li>{blog.content}</li>
                         </ul>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     )
