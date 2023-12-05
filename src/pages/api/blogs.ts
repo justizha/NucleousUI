ae7d0dce7c69afc0343b3e3c1d7c3fc7ba73df75
justizha <alfathizhaaaaaaa@gmail.com>
2023-12-05 15:38:33 +0700

    blogs api get request

--- src/pages/api/blogs.ts
+++ src/pages/api/blogs.ts
@@ -2,10 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../lib/prisma'
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
-        const blogs = await prisma.blog.findMany()
-        return res.status(200).json(blogs)
-    } else {
-        console.log("something went wrong")
-    }
+    const resultBlogs = await prisma.blog.findMany()
+    return res.json(resultBlogs)
 }
