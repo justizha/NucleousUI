cd31118aee6fdf8cbb6e47d8b3fe68f5e637c119
justizha <alfathizhaaaaaaa@gmail.com>
2023-12-05 15:24:22 +0700

    get request

--- src/pages/api/blogs.ts
+++ src/pages/api/blogs.ts
@@ -1,14 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../lib/prisma'
 
-export default async function handle(
-    req: NextApiRequest,
-    res: NextApiResponse,
-) {
-    const result = await prisma.blog.create({
-        data: {
-            ...req.body,
-        },
-    })
-    return res.status(201).json(result)
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'GET') {
+        const blogs = await prisma.blog.findMany()
+        return res.status(200).json(blogs)
+    } else {
+        console.log("something went wrong")
+    }
 }
