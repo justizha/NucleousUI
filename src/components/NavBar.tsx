0c26fde5b14b9883401b0019119049157ef24fd9
justizha <alfathizhaaaaaaa@gmail.com>
2023-12-27 16:23:36 +0700

    style : navbar z index

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -22,7 +22,7 @@ export default function NavBar() {
     const path = usePathname()
 
     return (
-        <header className='w-full h-16 border-b flex justify-between border-main-accent fixed z-50 bg-main-black '>
+        <header className='w-full h-16 border-b flex justify-between border-main-accent fixed  bg-main-black '>
             <ul className="flex items-center gap-8">
                 <li>
                     <Link href={'/'} className='font-semibold text-[30px] ml-2 flex gap-1 items-center cursor-pointer text-white'>
