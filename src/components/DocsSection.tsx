41218962d9ddf66194eb61983227512d81c35cb4
justizha <aflathizhaaaaaaa@gmail.com>
2024-03-01 10:14:00 +0700

    fix : changed div to section

--- src/components/DocsSection.tsx
+++ src/components/DocsSection.tsx
@@ -1,7 +1,7 @@
 export default function DocsSection({ children }) {
     return (
-        <div className="lg:pt-24 pt-32 lg:ml-80 border-main-accent max-w-5xl px-4">
+        <section className="lg:pt-24 pt-32 lg:ml-80 border-main-accent max-w-5xl px-4">
             {children}
-        </div>
+        </section>
     )
 }
