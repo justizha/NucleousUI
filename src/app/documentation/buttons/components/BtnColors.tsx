fb80f3c55bb699bf84ac7d9cd93303df55764427
justizha <alfathizhaaaaaaa@gmail.com>
2024-06-11 12:45:16 +0700

    refactors : format and spacing

--- src/app/documentation/buttons/components/BtnColors.tsx
+++ src/app/documentation/buttons/components/BtnColors.tsx
@@ -27,19 +27,34 @@ export default function BtnColors() {
       <p className="text-main-white/60">Different colors of buttons</p>
       <div className="mb-4 mt-4 max-w-xl rounded-md border border-main-accent">
         <div className="mb-3 flex h-32 max-w-xl items-center justify-center gap-3">
-          <button className="rounded-lg bg-white px-4 py-2 font-medium">
+          <button
+            type="button"
+            className="rounded-lg bg-white px-4 py-2 font-medium"
+          >
             Button
           </button>
-          <button className="rounded-lg bg-gray-500 px-4 py-2 font-medium text-white hover:bg-gray-600">
+          <button
+            type="button"
+            className="rounded-lg bg-gray-500 px-4 py-2 font-medium text-white hover:bg-gray-600"
+          >
             Button
           </button>
-          <button className="rounded-lg bg-green-500 px-4 py-2 font-medium text-white hover:bg-green-600">
+          <button
+            type="button"
+            className="rounded-lg bg-green-500 px-4 py-2 font-medium text-white hover:bg-green-600"
+          >
             Button
           </button>
-          <button className="rounded-lg bg-red-500 px-4 py-2 font-medium text-white hover:bg-red-600">
+          <button
+            type="button"
+            className="rounded-lg bg-red-500 px-4 py-2 font-medium text-white hover:bg-red-600"
+          >
             Button
           </button>
-          <button className="rounded-lg bg-yellow-500 px-4 py-2 font-medium text-white hover:bg-yellow-600">
+          <button
+            type="button"
+            className="rounded-lg bg-yellow-500 px-4 py-2 font-medium text-white hover:bg-yellow-600"
+          >
             Button
           </button>
         </div>
