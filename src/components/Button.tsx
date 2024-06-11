217a8746c5220edc932666575c0a915e66af1f8b
justizha <alfathizhaaaaaaa@gmail.com>
2024-06-11 09:59:12 +0700

    refactor & packages update

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link"
 
-export default function Button(props: any) {
+export default function Button(props) {
     return (
         <Link href={'/design'} {...props} className="text-lg border rounded p-2 text-main-black bg-main-white  duration-200 cursor-pointer hover:scale-95">
             {props.children}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,81 +5,100 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 export default function NavBar() {
-    const [isOpen, setIsOpen] = useState(false);
-    const menu = "menu.svg";
-    const close = "close.svg";
-    const NavLink = [
-        { href: "/documentation", title: "Docs" },
-        { href: "/underconstructions", title: "Example" },
-        { href: "/#feedback", title: "Feedback" }
-    ]
+  const [isOpen, setIsOpen] = useState(false);
+  const menu = "menu.svg";
+  const path = usePathname();
+  const close = "close.svg";
+  const NavLink = [
+    { href: "/documentation", title: "Docs" },
+    { href: "/underconstructions", title: "Example" },
+    { href: "/#feedback", title: "Feedback" },
+  ];
 
-    const path = usePathname()
-
-    return (
-        <>
-            {path.startsWith('/documentation') ? (
-                <></>
-            ) : (
-
-                <header className='w-full h-12 border-b flex justify-between border-main-accent fixed  bg-main-black '>
-                    <ul className="flex items-center gap-8">
-                        <li>
-                            <Link href={'/'} className='font-semibold text-[22px] lg:text-[28px] ml-2 flex gap-1 items-center cursor-pointer text-white'>
-                                <LayoutGrid className="text-main-blue" /> NucleousUI
-                            </Link>
-                        </li>
-                        {NavLink.map((link) => (
-                            <li key={link.href} className="md:flex hidden">
-                                <Link href={link.href} className={`text-gray-400 relative hover:text-gray-200 duration-150 text-sm ${link.href === path ? 'activeLink' : ''}`} >
-                                    {link.href === path && (
-                                        <motion.span
-                                            layoutId="underline"
-                                            className="absolute left-0 top-full mt-1 h-[1px] bg-main-white w-full" />
-                                    )}
-                                    {link.title}
-                                </Link>
-                            </li>
-                        ))}
+  return (
+    <>
+      {path.startsWith("/documentation") ? (
+        <></>
+      ) : (
+        <header className="fixed flex h-12 w-full justify-between border-b border-main-accent  bg-main-black ">
+          <ul className="flex items-center gap-8">
+            <li>
+              <Link
+                href={"/"}
+                className="ml-2 flex cursor-pointer items-center gap-1 text-[22px] font-semibold text-white lg:text-[28px]"
+              >
+                <LayoutGrid className="text-main-blue" /> NucleousUI
+              </Link>
+            </li>
+            {NavLink.map((link) => (
+              <li key={link.href} className="hidden md:flex">
+                <Link
+                  href={link.href}
+                  className={`relative text-sm text-gray-400 duration-150 hover:text-gray-200 ${link.href === path ? "activeLink" : ""}`}
+                >
+                  {link.href === path && (
+                    <motion.span
+                      layoutId="underline"
+                      className="absolute left-0 top-full mt-1 h-[1px] w-full bg-main-white"
+                    />
+                  )}
+                  {link.title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="flex flex-1 items-center justify-end md:hidden ">
+            <div className="mx-4 flex flex-1 items-center justify-end md:hidden">
+              <div className="grid place-items-center ">
+                <button
+                  type="button"
+                  onClick={() => {
+                    setIsOpen(!isOpen);
+                  }}
+                  className="flex h-10 w-10 items-center justify-center"
+                >
+                  <img
+                    src={menu}
+                    alt="menu"
+                    className=" duration 700 mt-3 w-[35px] cursor-pointer p-2  transition-opacity "
+                  />
+                </button>
+              </div>
+              {isOpen && (
+                // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
+                <div
+                  className="fixed inset-0 z-[200] bg-main-white bg-opacity-20 backdrop-blur-sm lg:hidden"
+                  onClick={() => {
+                    setIsOpen(!isOpen);
+                  }}
+                />
+              )}
+              {isOpen && (
+                <div className="fixed left-0 right-0 top-10 z-[200]  mx-4 h-1/2 rounded-lg bg-main-black py-5 shadow-lg">
+                  {NavLink.map((link) => (
+                    <ul
+                      className="mx-4 mt-1 flex list-none flex-col justify-center gap-1 text-start"
+                      key={link.href}
+                    >
+                      <li>
+                        <Link
+                          href={link.href}
+                          className="flex max-w-sm gap-1 px-4 py-2 text-base text-main-white  duration-150 hover:bg-opacity-70 hover:text-gray-200"
+                          onClick={() => {
+                            setIsOpen(!isOpen);
+                          }}
+                        >
+                          {link.title}
+                        </Link>
+                      </li>
                     </ul>
-                    <div className="md:hidden flex flex-1 justify-end items-center ">
-                        <div className="md:hidden flex flex-1 justify-end items-center mx-4">
-                            <div className="grid place-items-center ">
-                                <button onClick={() => { setIsOpen(!isOpen) }} className='flex items-center justify-center h-10 w-10'>
-                                    <img
-                                        src={menu}
-                                        alt="menu"
-                                        className=' w-[35px] cursor-pointer transition-opacity duration 700 p-2  mt-3 '
-                                    />
-                                </button>
-                            </div>
-                            {isOpen && (
-                                <div
-                                    className="fixed inset-0 bg-main-white backdrop-blur-sm bg-opacity-20 z-[200] lg:hidden"
-                                    onClick={() => { setIsOpen(!isOpen) }}
-                                ></div>
-                            )}
-                            {isOpen && (
-                                <div className='fixed top-10 right-0 left-0 mx-4  shadow-lg h-1/2 rounded-lg bg-main-black z-[200] py-5'>
-                                    {NavLink.map((link) => (
-                                        <ul className="list-none mx-4 text-start flex justify-center flex-col mt-1 gap-1" key={link.href}>
-                                            <li >
-                                                <Link
-                                                    href={link.href}
-                                                    className='text-base flex gap-1 text-main-white hover:bg-opacity-70 hover:text-gray-200 duration-150  max-w-sm px-4 py-2' onClick={() => { setIsOpen(!isOpen) }}>
-                                                    {link.title}
-                                                </Link>
-                                            </li>
-                                        </ul>
-                                    ))}
-                                </div>
-                            )}
-                        </div>
-                    </div>
-
-                </header >
-            )}
-        </>
-
-    )
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        </header>
+      )}
+    </>
+  );
 }
